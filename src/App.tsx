28ba040bd35f2dab27bfc4718d32df7af39747d8
Eddie Colon <jamal@Eddies-MacBook-Pro.local>
2021-01-28 23:56:35 -0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card } from './Card';
+import { Column } from './Column';
+
+import { AppContainer, ColumnTitle} from './styles'
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      
+       <Column text="To DO">
+         <Card text="Generate app sccaffold"/>
+       </Column>
+
+        <Column text="Typescript"> 
+            <Card text="Learn Typescript"/>
+        </Column>
+    </AppContainer>
   );
 }
 
---
+++ src/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CardContainer } from './styles'
+
+
+interface CardProps{
+    text: string;
+}
+
+export const Card: React.FC<CardProps> = ({text})=>{
+ return <CardContainer>
+     {text}
+ </CardContainer>
+}
