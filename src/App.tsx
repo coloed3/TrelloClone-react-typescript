4c58925430428f4126035a4dd03850fb836499ee
Eddie Colon <coloed3@gmail.com>
2021-02-05 14:00:20 -0700

    added context for the cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React, { useReducer } from "react";
+import React from "react";
 import { AddNewItem } from "./AddNewItem";
 import { Card } from "./Card";
 import { Column } from "./Column";
 
 import { AppContainer } from "./styles";
-import { counterReducer } from "./counterReducer";
+import { useAppState } from "./AppStateContext";
 function App() {
-  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+  const { state } = useAppState();
   return (
     <AppContainer>
-      <Column text='To DO'>
-        <Card text='Generate app sccaffold' />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
 
-      <Column text='Typescript'>
-        <Card text='Learn Typescript' />
-      </Column>
       <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log} />
-      <p> Count: {state.count} </p>
-      <button onClick={() => dispatch({ type: "decrement" })}>- </button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
     </AppContainer>
   );
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { AddNewItem } from "./AddNewItem";
 import { ColumnContainer, ColumnTitle } from "./styles";
+import { useAppState } from "./AppStateContext";
+import { Card } from "./Card";
 
 interface ColumnProps {
   text: string;
-  children: React.ReactNode;
+  index: number;
 }
 
-export const Column: React.FC<ColumnProps> = ({ text, children }) => {
+export const Column: React.FC<ColumnProps> = ({ text, index }) => {
+  const { state } = useAppState();
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
-
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
       <AddNewItem
         toggleButtonText='+ Add another task'
         onAdd={console.log}
